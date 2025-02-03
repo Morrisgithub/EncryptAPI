f7cf2a4051308f0415f553fa6f3468d899bbafd3
Moris Afram <moris.afram@learnet.se>
2025-02-03 19:34:28 +0100

    first commit

---
+++ CryptoAPI/Controllers/Kryptering.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EncryptionController : ControllerBase
+{
+    private readonly EncryptionService _encryptionService;
+
+    public EncryptionController()
+    {
+        _encryptionService = new EncryptionService();  // Instansiera EncryptionService
+    }
+
+    // POST api/encryption/encrypt
+    [HttpPost("encrypt")]
+    public ActionResult<string> Encrypt([FromBody] string text)
+    {
+        if (string.IsNullOrEmpty(text))
+        {
+            return BadRequest("Text must not be empty.");
+        }
+
+        var encryptedText = _encryptionService.Encrypt(text);
+        return Ok(new { encrypted_text = encryptedText });
+    }
+
+    // POST api/encryption/decrypt
+    [HttpPost("decrypt")]
+    public ActionResult<string> Decrypt([FromBody] string text)
+    {
+        if (string.IsNullOrEmpty(text))
+        {
+            return BadRequest("Text must not be empty.");
+        }
+
+        var decryptedText = _encryptionService.Decrypt(text);
+        return Ok(new { decrypted_text = decryptedText });
+    }
+}
---
+++ CryptoAPI/EncryptionService.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+public class EncryptionService
+{
+    // Enkel krypteringsmetod (Rövarspråket)
+    public string Encrypt(string text)
+    {
+        StringBuilder encryptedText = new StringBuilder();
+        foreach (var c in text)
+        {
+            if ("aeiouåäö".Contains(char.ToLower(c)))
+            {
+                encryptedText.Append(c);
+                encryptedText.Append('o');
+                encryptedText.Append(char.ToLower(c));
+            }
+            else
+            {
+                encryptedText.Append(c);
+            }
+        }
+        return encryptedText.ToString();
+    }
+
+    // Enkel avkrypteringsmetod (Rövarspråket)
+    public string Decrypt(string text)
+    {
+        StringBuilder decryptedText = new StringBuilder();
+        int i = 0;
+        while (i < text.Length)
+        {
+            decryptedText.Append(text[i]);
+            if ("aeiouåäö".Contains(char.ToLower(text[i])) && i + 2 < text.Length && text[i + 2] == text[i])
+            {
+                i += 3;  // Hoppa över 'o' och den upprepade bokstaven
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return decryptedText.ToString();
+    }
+}
